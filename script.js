df8ff01dfbda70687d73f72f377fa4a74b6e7fbd
lera <lw.graffova@gmail.com>
2024-11-02 23:53:22 +0300

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function()
+{
+    const name = "Графова Валерияgit ";
+    const element = document.querySelector("#name");
+    if (element)
+    {
+        element.textContent = name;
+    }
+}
